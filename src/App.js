e1dfa8808272f8eae60f7888a1dfe8868250f190
Pratik Lunagaria <pratiklunagaria@hotmail.com>
2018-10-15 13:28:37 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Chart from 'react-apexcharts';
+import { Link, BrowserRouter, Switch, Route} from 'react-router-dom';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: {
+        chart: {
+          id: 'apexchart-example',
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018]
+        }
+      },
+      series: [{
+        name: 'series-1',
+        data: [  45, 50, 49, 60, 70, 91,112,323,221,221,444,544,211,334,1212,434,232,566,343,665,334,887]
+      },
+      {
+        name: 'series-2',
+        data: [49, 60, 70, 91,112,323,221,221,444,544,211,334,1212,434,232,566,343,665,334,887]
+      },
+      {
+        name: 'series-3',
+        data: [30, 40, 45, 50, 49, 60, 70, 91,112,323,221,221,444,544,211,334,1212,434,232,566,343,665,334,887]
+      }
+    ]
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <Chart  
+        className="container" 
+        options={this.state.options} 
+        series={this.state.series} 
+        type="scatter"  
+        />
       </div>
-    );
+    )
   }
 }
 
